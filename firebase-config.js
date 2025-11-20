75da848a6bbfb4ebc556e63d2ba8a19f9c25b6a9
hrju-hash <hr.ju@malgnsoft.com>
2025-11-20 15:35:25 +0900

    Firebase 援ъ꽦 ?뺣낫 異붽?

--- firebase-config.js
+++ firebase-config.js
@@ -9,15 +9,13 @@
 // 5. 아래 firebaseConfig 객체의 값들을 실제 값으로 변경
 
 const firebaseConfig = {
-    // TODO: 아래 정보를 실제 Firebase 프로젝트 정보로 변경하세요
-    // Firebase 콘솔 > 프로젝트 설정 > 일반 > 앱 > 웹 앱에서 복사
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.asia-northeast3.firebasedatabase.app",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBXvm6xweVo83OIBfILDOKZfwv1nk4tXk0",
+    authDomain: "malgn-booking.firebaseapp.com",
+    databaseURL: "https://malgn-booking-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "malgn-booking",
+    storageBucket: "malgn-booking.firebasestorage.app",
+    messagingSenderId: "768220823948",
+    appId: "1:768220823948:web:a820f41e7f19da314056a9"
 };
 
 // Firebase 초기화
